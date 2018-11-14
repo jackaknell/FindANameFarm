47754ae1475c6761351cc05433fc87b25aded42c
Jason Pinner <399002@edu.cwa.ac.uk>
2018-11-14 16:22:18 +0000

    Fields and Treatment forms connected

--- FindANameFarm/Forms/FieldsForm.Designer.cs
+++ FindANameFarm/Forms/FieldsForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace FindANameFarm.Forms
+﻿using System;
+
+namespace FindANameFarm.Forms
 {
     partial class FieldsForm
     {
@@ -28,172 +30,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(175, 139);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 20);
-            this.textBox2.TabIndex = 65;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 142);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
-            this.label1.TabIndex = 64;
-            this.label1.Text = "Field Suitablity";
-            // 
-            // listView1
-            // 
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(317, 41);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(424, 281);
-            this.listView1.TabIndex = 63;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(175, 41);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(121, 20);
-            this.textBox3.TabIndex = 62;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(60, 44);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(18, 13);
-            this.label2.TabIndex = 61;
-            this.label2.Text = "ID";
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(221, 299);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 60;
-            this.button4.Text = "Delete";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(221, 269);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 59;
-            this.button3.Text = "Update";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(640, 387);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 58;
-            this.button2.Text = "Close";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(221, 240);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 57;
-            this.button1.Text = "Create";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(175, 102);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(121, 20);
-            this.numericUpDown1.TabIndex = 56;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(175, 71);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 55;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(60, 74);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 54;
-            this.label4.Text = "Name";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(60, 104);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 13);
-            this.label3.TabIndex = 53;
-            this.label3.Text = "Field Size";
-            // 
-            // FieldsForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Name = "FieldsForm";
-            this.Text = "FieldsForm";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.txtFieldSuitability = new System.Windows.Forms.TextBox();
+			this.lblFieldSuitability = new System.Windows.Forms.Label();
+			this.listField = new System.Windows.Forms.ListView();
+			this.txtFieldId = new System.Windows.Forms.TextBox();
+			this.lblFieldId = new System.Windows.Forms.Label();
+			this.btnUpdateField = new System.Windows.Forms.Button();
+			this.btnFieldClose = new System.Windows.Forms.Button();
+			this.btnFieldCreate = new System.Windows.Forms.Button();
+			this.nudFieldSize = new System.Windows.Forms.NumericUpDown();
+			this.txtFieldName = new System.Windows.Forms.TextBox();
+			this.lblFieldName = new System.Windows.Forms.Label();
+			this.lblFieldSize = new System.Windows.Forms.Label();
+			this.btnFieldClear = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.nudFieldSize)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// txtFieldSuitability
+			// 
+			this.txtFieldSuitability.Location = new System.Drawing.Point(175, 139);
+			this.txtFieldSuitability.Name = "txtFieldSuitability";
+			this.txtFieldSuitability.Size = new System.Drawing.Size(121, 20);
+			this.txtFieldSuitability.TabIndex = 65;
+			// 
+			// lblFieldSuitability
+			// 
+			this.lblFieldSuitability.AutoSize = true;
+			this.lblFieldSuitability.Location = new System.Drawing.Point(60, 142);
+			this.lblFieldSuitability.Name = "lblFieldSuitability";
+			this.lblFieldSuitability.Size = new System.Drawing.Size(74, 13);
+			this.lblFieldSuitability.TabIndex = 64;
+			this.lblFieldSuitability.Text = "Field Suitablity";
+			// 
+			// listField
+			// 
+			this.listField.GridLines = true;
+			this.listField.Location = new System.Drawing.Point(317, 41);
+			this.listField.Name = "listField";
+			this.listField.Size = new System.Drawing.Size(424, 281);
+			this.listField.TabIndex = 63;
+			this.listField.UseCompatibleStateImageBehavior = false;
+			this.listField.View = System.Windows.Forms.View.Details;
+			this.listField.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listField_MouseClick_1);
+			// 
+			// txtFieldId
+			// 
+			this.txtFieldId.Location = new System.Drawing.Point(175, 41);
+			this.txtFieldId.Name = "txtFieldId";
+			this.txtFieldId.ReadOnly = true;
+			this.txtFieldId.Size = new System.Drawing.Size(121, 20);
+			this.txtFieldId.TabIndex = 62;
+			// 
+			// lblFieldId
+			// 
+			this.lblFieldId.AutoSize = true;
+			this.lblFieldId.Location = new System.Drawing.Point(60, 44);
+			this.lblFieldId.Name = "lblFieldId";
+			this.lblFieldId.Size = new System.Drawing.Size(18, 13);
+			this.lblFieldId.TabIndex = 61;
+			this.lblFieldId.Text = "ID";
+			// 
+			// btnUpdateField
+			// 
+			this.btnUpdateField.Location = new System.Drawing.Point(221, 297);
+			this.btnUpdateField.Name = "btnUpdateField";
+			this.btnUpdateField.Size = new System.Drawing.Size(75, 23);
+			this.btnUpdateField.TabIndex = 59;
+			this.btnUpdateField.Text = "Update";
+			this.btnUpdateField.UseVisualStyleBackColor = true;
+			this.btnUpdateField.Click += new System.EventHandler(this.btnUpdateField_Click);
+			// 
+			// btnFieldClose
+			// 
+			this.btnFieldClose.Location = new System.Drawing.Point(640, 387);
+			this.btnFieldClose.Name = "btnFieldClose";
+			this.btnFieldClose.Size = new System.Drawing.Size(75, 23);
+			this.btnFieldClose.TabIndex = 58;
+			this.btnFieldClose.Text = "Close";
+			this.btnFieldClose.UseVisualStyleBackColor = true;
+			this.btnFieldClose.Click += new System.EventHandler(this.btnFieldClose_Click);
+			// 
+			// btnFieldCreate
+			// 
+			this.btnFieldCreate.Location = new System.Drawing.Point(221, 268);
+			this.btnFieldCreate.Name = "btnFieldCreate";
+			this.btnFieldCreate.Size = new System.Drawing.Size(75, 23);
+			this.btnFieldCreate.TabIndex = 57;
+			this.btnFieldCreate.Text = "Create";
+			this.btnFieldCreate.UseVisualStyleBackColor = true;
+			this.btnFieldCreate.Click += new System.EventHandler(this.btnFieldCreate_Click);
+			// 
+			// nudFieldSize
+			// 
+			this.nudFieldSize.Location = new System.Drawing.Point(175, 102);
+			this.nudFieldSize.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+			this.nudFieldSize.Name = "nudFieldSize";
+			this.nudFieldSize.Size = new System.Drawing.Size(121, 20);
+			this.nudFieldSize.TabIndex = 56;
+			// 
+			// txtFieldName
+			// 
+			this.txtFieldName.Location = new System.Drawing.Point(175, 71);
+			this.txtFieldName.Name = "txtFieldName";
+			this.txtFieldName.Size = new System.Drawing.Size(121, 20);
+			this.txtFieldName.TabIndex = 55;
+			// 
+			// lblFieldName
+			// 
+			this.lblFieldName.AutoSize = true;
+			this.lblFieldName.Location = new System.Drawing.Point(60, 74);
+			this.lblFieldName.Name = "lblFieldName";
+			this.lblFieldName.Size = new System.Drawing.Size(35, 13);
+			this.lblFieldName.TabIndex = 54;
+			this.lblFieldName.Text = "Name";
+			// 
+			// lblFieldSize
+			// 
+			this.lblFieldSize.AutoSize = true;
+			this.lblFieldSize.Location = new System.Drawing.Point(60, 104);
+			this.lblFieldSize.Name = "lblFieldSize";
+			this.lblFieldSize.Size = new System.Drawing.Size(52, 13);
+			this.lblFieldSize.TabIndex = 53;
+			this.lblFieldSize.Text = "Field Size";
+			// 
+			// btnFieldClear
+			// 
+			this.btnFieldClear.Location = new System.Drawing.Point(221, 178);
+			this.btnFieldClear.Name = "btnFieldClear";
+			this.btnFieldClear.Size = new System.Drawing.Size(75, 23);
+			this.btnFieldClear.TabIndex = 66;
+			this.btnFieldClear.Text = "Clear";
+			this.btnFieldClear.UseVisualStyleBackColor = true;
+			this.btnFieldClear.Click += new System.EventHandler(this.btnFieldClear_Click);
+			// 
+			// FieldsForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnFieldClear);
+			this.Controls.Add(this.txtFieldSuitability);
+			this.Controls.Add(this.lblFieldSuitability);
+			this.Controls.Add(this.listField);
+			this.Controls.Add(this.txtFieldId);
+			this.Controls.Add(this.lblFieldId);
+			this.Controls.Add(this.btnUpdateField);
+			this.Controls.Add(this.btnFieldClose);
+			this.Controls.Add(this.btnFieldCreate);
+			this.Controls.Add(this.nudFieldSize);
+			this.Controls.Add(this.txtFieldName);
+			this.Controls.Add(this.lblFieldName);
+			this.Controls.Add(this.lblFieldSize);
+			this.Name = "FieldsForm";
+			this.Text = "FieldsForm";
+			this.Load += new System.EventHandler(this.FieldsForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.nudFieldSize)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
-        #endregion
+		
 
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-    }
+		#endregion
+
+		private System.Windows.Forms.TextBox txtFieldSuitability;
+        private System.Windows.Forms.Label lblFieldSuitability;
+        private System.Windows.Forms.ListView listField;
+        private System.Windows.Forms.TextBox txtFieldId;
+        private System.Windows.Forms.Label lblFieldId;
+        private System.Windows.Forms.Button btnUpdateField;
+        private System.Windows.Forms.Button btnFieldClose;
+        private System.Windows.Forms.Button btnFieldCreate;
+        private System.Windows.Forms.NumericUpDown nudFieldSize;
+        private System.Windows.Forms.TextBox txtFieldName;
+        private System.Windows.Forms.Label lblFieldName;
+        private System.Windows.Forms.Label lblFieldSize;
+		private System.Windows.Forms.Button btnFieldClear;
+	}
 }
